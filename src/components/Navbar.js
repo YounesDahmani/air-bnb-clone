bb41133974435c59dd8c8856a6c7cb8c88a345f3
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-09-20 16:53:49 +0200

    Modification of the navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import LanguageIcon from "@mui/icons-material/Language";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="logo-desktop">
+          <img src="../assets/img/air-bnb-logo.png" alt="Logo Airbnb" />
+        </div>
+        <div className="search-features">search features</div>
+        <div className="user-features">
+          <a href="https://www.google.com" className="hote-btn">
+            Devenez hôte
+          </a>
+          <LanguageIcon />
+          <div className="user-account-btn">
+            <MenuIcon />
+            <AccountCircleIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
