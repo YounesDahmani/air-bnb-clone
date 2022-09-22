d5e9765f416bed31a11f4647d071d9cf9d42bfd8
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-09-22 12:36:42 +0200

    realization of scss for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import LanguageIcon from "@mui/icons-material/Language";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SearchIcon from "@mui/icons-material/Search";
 const Navbar = () => {
   return (
     <div className="navbar">
@@ -9,7 +10,16 @@ const Navbar = () => {
         <div className="logo-desktop">
           <img src="../assets/img/air-bnb-logo.png" alt="Logo Airbnb" />
         </div>
-        <div className="search-features">search features</div>
+
+        {/* Middle feature  */}
+        <div className="search-features">
+          <div className="location-btn">N'importe où</div>
+          <div className="day-btn">Une Semaine</div>
+          <div className="travelers-btn">
+            <span>Ajouter des voyageurs</span>{" "}
+            <SearchIcon className="search-icon" />{" "}
+          </div>
+        </div>
         <div className="user-features">
           <a href="https://www.google.com" className="hote-btn">
             Devenez hôte
