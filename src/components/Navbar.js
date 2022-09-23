7bb68e9b1c7776846864ce0dcf8b7937c1680d64
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-09-23 17:53:01 +0200

    Add state to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import LanguageIcon from "@mui/icons-material/Language";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SearchIcon from "@mui/icons-material/Search";
 const Navbar = () => {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = () => {
+    setIsShown((isShown) => !isShown);
+  };
+  let toggleClassCheck = isShown ? " active" : "";
+
   return (
-    <div className="navbar">
+    <div className={`navbar${toggleClassCheck}`}>
       <div className="navbar-container">
         <div className="logo-desktop">
           <img src="../assets/img/air-bnb-logo.png" alt="Logo Airbnb" />
         </div>
 
         {/* Middle feature  */}
-        <div className="search-features">
-          <div className="location-btn">N'importe où</div>
+        {isShown && (
+          <div>
+            <ul>
+              <li>Logements</li>
+              <li>Expériences</li>
+              <li>Expériences en ligne</li>
+            </ul>
+          </div>
+        )}
+        <div className={`search-features${toggleClassCheck}`}>
+          <div className="location-btn" onClick={handleClick}>
+            N'importe où
+          </div>
           <div className="day-btn">Une Semaine</div>
           <div className="travelers-btn">
             <span>Ajouter des voyageurs</span>{" "}
-            <SearchIcon className="search-icon" />{" "}
+            <div className="icon-search">
+              <SearchIcon />{" "}
+            </div>
           </div>
         </div>
         <div className="user-features">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Home</h1>
     </div>
   );
 };
